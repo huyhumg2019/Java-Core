a4179da61af17fc407b5371036db5bfad9a6be1f
huy nguyen <huyhumg2019@gmail.com>
2019-07-09 02:42:49 +0900

    ok

---
+++ Fibonacci.java
@@ -0,0 +1,19 @@
+public class Fibonacci{
+ 
+    public static void main(String[] args) {
+        System.out.print("Day fibonacci :" );
+        for(int i=0;i<10;i++){
+            System.out.print(fibonacci(i)+ " ");
+
+        }
+        
+    
+        }
+        public static int fibonacci(int n){
+            if(n<0){
+                
+            }
+            return n;
+    }
+    
+}
