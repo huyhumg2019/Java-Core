b7f59aec6a372b6d80cfcbc827984f7147e899cd
huy nguyen <huyhumg2019@gmail.com>
2019-07-09 02:51:05 +0900

    fibonacci

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,9 +11,13 @@ public class Fibonacci{
         }
         public static int fibonacci(int n){
             if(n<0){
-                
+                return -1;
+            } else if(n==0||n==1){
+                return n;
+            } else{
+                return fibonacci(n-2)+fibonacci(n-1);
             }
-            return n;
+            
     }
     
 }
