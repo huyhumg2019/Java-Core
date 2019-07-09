4286714d344ea7243ff1b22ad6d644a2ea6c2868
huy nguyen <huyhumg2019@gmail.com>
2019-07-09 18:55:38 +0900

    so nguyen to

---
+++ SoNguyenTo.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class SoNguyenTo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Nhap Day So Can Kiem Tra");
+        int n = s.nextInt();
+        if(isPrime(n)){
+            System.out.println(n+" la so nguyen to");
+
+
+        }else{
+            System.out.println(n+"ko phai la so nguyen to");
+        }
+    }
+    public static boolean isPrime(int n){
+        if(n<2){
+            return false;
+        }
+        for(int i = 2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+            
+        }
+
+        return true;
+
+    }
+}
